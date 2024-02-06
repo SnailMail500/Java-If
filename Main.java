ffb8c067120e2282ce0f2d8d9deb51adcd1e29a7
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-06 17:25:10 +0000

    Added new comment on else if statements

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ public class Main {
         if (test == true){
             System.out.println("This time, it's the if that will never be met (this bit here)");
         }
+        //could have an else if on this line with a different condition in case that's a thing you really want to do (it's fine I'm just snarky)
         else{
             System.out.println("This statement will be executed!");
         }
